d2ef0ff59b14d4013da53ba8c0340319f972fb4e
Godfred Kogkane <123625779+godfredkogkane@users.noreply.github.com>
2024-04-09 20:13:56 +0000

    made edits to submit review and results

--- action/submit_review.php
+++ action/submit_review.php
@@ -13,19 +13,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $review_text = $_POST['review'];
     $user_id = $_SESSION['user_id']; // Get the UserID from the session
 
-    // Retrieve username from Users table based on UserID
-    $username_query = "SELECT Username FROM Users WHERE UserID = ?";
-    $stmt_username = $con->prepare($username_query);
-    $stmt_username->bind_param("i", $user_id);
-    $stmt_username->execute();
-    $stmt_username->bind_result($username);
-    $stmt_username->fetch();
-    $stmt_username->close();
-
-    // Insert review into Reviews table with the UserID and Username
-    $insert_review_query = "INSERT INTO Reviews (UserID, Username, DocumentID, Rating, ReviewText) VALUES (?, ?, ?, ?, ?)";
+    // Insert review into Reviews table with the UserID
+    $insert_review_query = "INSERT INTO Reviews (UserID, DocumentID, Rating, ReviewText) VALUES (?, ?, ?, ?)";
     $stmt = $con->prepare($insert_review_query);
-    $stmt->bind_param("isis", $user_id, $username, $document_id, $rating, $review_text);
+    $stmt->bind_param("iiss", $user_id, $document_id, $rating, $review_text);
 
     if ($stmt->execute() === TRUE) {
         echo "Review submitted successfully.";
