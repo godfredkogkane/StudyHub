c479190504066304b59f5648326225f03feefea4
Godfred Kogkane <123625779+godfredkogkane@users.noreply.github.com>
2024-04-09 00:40:05 +0000

    made edits to review page and search css

--- view/review.php
+++ view/review.php
@@ -6,6 +6,8 @@
     <title>Document Reviews</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/review.css">
+
 </head>
 <body>
     <!-- Navigation -->
