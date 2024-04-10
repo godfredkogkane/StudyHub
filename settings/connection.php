b275c91727f84aa78b8a6ece1baced4d4fcf66e1
Godfred Kogkane <123625779+godfredkogkane@users.noreply.github.com>
2024-04-10 20:38:53 +0000

    added new connection

--- settings/connection.php
+++ settings/connection.php
@@ -17,14 +17,28 @@
 // }
 
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-?>
+define('DB_HOST', 'us-cluster-east-01.k8s.cleardb.net');
+define('DB_USER', 'b2fe95fc104b1f');
+define('DB_PASSWORD', 'af0534e5'); 
+define('DB_NAME', 'heroku_3718b5dd7edcc36');
+
+
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+} 
+
+
+
+// //Get Heroku ClearDB connection information
+// $cleardb_url = parse_url(getenv("mysql://b2fe95fc104b1f:af0534e5@us-cluster-east-01.k8s.cleardb.net/heroku_3718b5dd7edcc36?reconnect=true"));
+// $cleardb_server = $cleardb_url["us-cluster-east-01.k8s.cleardb.net"];
+// $cleardb_username = $cleardb_url["b2fe95fc104b1f"];
+// $cleardb_password = $cleardb_url["af0534e5"];
+// $cleardb_db = substr($cleardb_url["heroku_3718b5dd7edcc36"],1);
+// $active_group = 'default';
+// $query_builder = TRUE;
+// // Connect to DB
+// $con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// ?>
